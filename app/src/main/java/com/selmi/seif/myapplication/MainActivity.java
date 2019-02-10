621242373df846460d26f8809ddcf8f0bc753ab3
Seifeddine Selmi <selmi.seifeddine19@gmail.com>
2019-02-09 22:44:24 -0500

    Test display text in virtual  machine Android Emulator

--- app/src/main/java/com/selmi/seif/myapplication/MainActivity.java
+++ app/src/main/java/com/selmi/seif/myapplication/MainActivity.java
@@ -2,12 +2,18 @@ package com.selmi.seif.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        /* setContentView(R.layout.activity_main); */
+
+        // Test view with text
+        TextView text = new TextView(this);
+        text.setText("Bonjour, vous me devez 1 000 000€.");
+        setContentView(text);
     }
 }
