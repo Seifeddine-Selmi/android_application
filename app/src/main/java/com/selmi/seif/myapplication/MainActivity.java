abf701944f49f8437e2b0b2d7063ca30a93ea164
Seifeddine Selmi <selmi.seifeddine19@gmail.com>
2019-02-10 21:38:31 -0500

    Manipulate class R.java to get xml resource

--- app/src/main/java/com/selmi/seif/myapplication/MainActivity.java
+++ app/src/main/java/com/selmi/seif/myapplication/MainActivity.java
@@ -6,14 +6,31 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView text = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         /* setContentView(R.layout.activity_main); */
 
         // Test view with text
-        TextView text = new TextView(this);
+       /* TextView text = new TextView(this);
         text.setText("Bonjour, vous me devez 1 000 000€.");
-        setContentView(text);
+        setContentView(text);*/
+
+        // Use Class R to call resource string attribute app_name
+       /* text = new TextView(this);
+        text.setText(R.string.app_name);
+        setContentView(text);*/
+
+       // Update R.string.app_name with getResources
+        String applicationName = getResources().getString(R.string.app_name);
+        // Au lieu d'afficher "My Application" on va afficher "My way !"
+        applicationName = applicationName.replace("Application", "way !");
+
+        TextView view = new TextView(this);
+        view.setText(applicationName);
+
+        setContentView(view);
     }
 }
